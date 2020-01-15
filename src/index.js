a2b0a213048c1e6ffad59515e81bc10c92addd84
norzserge <metelskiisa@mail.ru>
2020-01-15 21:58:57 +0300

    Fixed bug with path of index.js in package.json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const moment = require("moment"); // import moment from 'moment';
+// below is an alternative solution from video: console.log(moment().format("DD/MM/YYYY HH:mm"));
 moment.locale("en-gb");
 console.log(moment().format("L") + " " + moment().format("LT"));
-// console.log(moment().format("DD/MM/YYYY HH:mm"));
